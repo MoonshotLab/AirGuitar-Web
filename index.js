565fc32e910bd8fce09886d31e7b06926214a2d3
Joe Longstreet <joelongstreet@gmail.com>
2014-07-11 17:49:24 -0500

    render views, make sockets a module, use promises

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 var path = require('path');
 var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http');
 var routes = require('./lib/routes');
+var sockets = require('./lib/sockets');
+
+var app = express();
+var server = http.Server(app);
 var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', routes.home);
+app.get('/', routes.allAwesome);
 app.get('/awesomes', routes.allAwesome);
 app.get('/awesome/:id', routes.findAwesome);
 app.post('/awesome', routes.makeAwesome);
 
-io.on('connection', function(socket){
-  console.log('connected');
-});
-
-http.listen(port, function(){
+server.listen(port, function(){
   console.log('listening on *:3000');
 });
+
+sockets.connect(server);
