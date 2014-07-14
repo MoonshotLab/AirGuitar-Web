f9713a3c4f4f8008481deea0b363fa2783857355
Joe Longstreet <joelongstreet@gmail.com>
2014-07-14 10:07:46 -0500

    accept posts from rig app to broadcast new socket events to clients

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser');
 var routes = require('./lib/routes');
 var sockets = require('./lib/sockets');
 
@@ -10,6 +11,9 @@ var port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.allAwesome);
