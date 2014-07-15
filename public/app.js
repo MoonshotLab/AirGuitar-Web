28615d80d0582abcc8dede1463859dc2736efc2f
Joe Longstreet <joelongstreet@gmail.com>
2014-07-14 21:23:06 -0500

    append templates upon socket notifications

--- public/app.js
+++ public/app.js
@@ -1,3 +1,33 @@
-var socket = io();
-var templates = {};
-var content = {};
+var makeAwesome = function(awesome){
+  var rendered = templates.awesome(awesome);
+  $('#content').prepend(rendered);
+};
+
+var makeSlowmo = function(slowmo){
+  var rendered = templates.slowmo(slowmo);
+  var selector = '#slowmo-' + slowmo.shortCode;
+
+  $('#content').prepend(rendered);
+  $(selector)[0].playbackRate = '.25';
+};
+
+
+socket.on('new-awesome', makeAwesome);
+socket.on('new-slowmo', makeSlowmo);
+
+
+$(function(){
+
+  if(content.awesomes){
+    content.awesomes.forEach(function(awesome){
+      makeAwesome(awesome);
+    });
+  }
+
+  if(content.slowmos){
+    content.slowmos.forEach(function(slowmo){
+      makeSlowmo(slowmo);
+    });
+  }
+
+});
