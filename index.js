882e0170fe534d95e0a0fa86b3a4967f11aef0c6
Joe Longstreet <joelongstreet@gmail.com>
2014-07-24 08:48:42 -0500

    use stylus

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.allAwesomes);
