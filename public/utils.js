c4168d05038696819f64946c6c2eefa7029fdea1
Joe Longstreet <joelongstreet@gmail.com>
2014-07-24 15:04:04 -0500

    spray random colors for debug mode

---
+++ public/utils.js
@@ -0,0 +1,26 @@
+var debugMode = false;
+var params = window.location.search.replace('?', '').split('&');
+
+if(params.indexOf('debug=true') != -1)
+  debugMode = true;
+
+
+$(function(){
+  if(debugMode){
+    var $units = $('.unit');
+
+    $.each($units, function(i, unit){
+      var color = [
+        'rgb(',
+          Math.floor(Math.random() * 255) + 1,
+          ',',
+          Math.floor(Math.random() * 255) + 1,
+          ',',
+          Math.floor(Math.random() * 255) + 1,
+        ')'
+      ].join('');
+      console.log($(unit));
+      $(unit).css('backgroundColor', color);
+    });
+  }
+});
